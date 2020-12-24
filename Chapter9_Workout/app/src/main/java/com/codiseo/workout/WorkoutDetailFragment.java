71ab586b20400580b04a8963e6abee49e17e60c6
alanquintero <alan_q_b@hotmail.com>
2020-12-24 15:22:26 -0800

    chapter 9 part 1

---
+++ Chapter9_Workout/app/src/main/java/com/codiseo/workout/WorkoutDetailFragment.java
@@ -0,0 +1,17 @@
+package com.codiseo.workout;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class WorkoutDetailFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_workout_detail, container, false);
+    }
+}

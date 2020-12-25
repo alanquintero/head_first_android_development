b1ff9f2d57c3b890f4c25959ec12e66d9244a0b9
alanquintero <alan_q_b@hotmail.com>
2020-12-25 14:33:08 -0800

    chapter 11 part 1

---
+++ Chapter11_Workout/app/src/main/java/com/codiseo/workout/WorkoutDetailFragment.java
@@ -0,0 +1,52 @@
+package com.codiseo.workout;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+public class WorkoutDetailFragment extends Fragment {
+    private static String WORKOUT_ID = "workoutId";
+    private long workoutId;
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if(savedInstanceState != null) {
+            workoutId = savedInstanceState.getLong(WORKOUT_ID);
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_workout_detail, container, false);
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        View view = getView();
+        if(view != null) {
+            TextView title = view.findViewById(R.id.textTitle);
+            Workout workout = Workout.workouts[(int) workoutId];
+            title.setText(workout.getName());
+            TextView description = view.findViewById(R.id.textDescription);
+            description.setText(workout.getDescription());
+        }
+    }
+
+    @Override
+    public void onSaveInstanceState(@NonNull Bundle savedInstanceState) {
+        savedInstanceState.putLong(WORKOUT_ID, workoutId);
+    }
+
+    public void setWorkout(long id) {
+        this.workoutId = id;
+    }
+}
